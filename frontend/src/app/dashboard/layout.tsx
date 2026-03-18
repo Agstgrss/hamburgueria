9feddbcbe67a1cd6e425567301c5c6d0c701f738
Agstgrss <augustograssi0101@gmail.com>
2026-03-18 15:29:17 -0300

    adjust auth 2

--- frontend/src/app/dashboard/layout.tsx
+++ frontend/src/app/dashboard/layout.tsx
@@ -6,25 +6,24 @@ export default async function dashboardLayout({
     children
 }: {
     children: React.ReactNode
-}){
+}) {
     const user = await requiredAdmin();
 
-    return(
-    <div className ="flex h-screen overflow-hidden text-white">
-        {/* Sidebar Desktop*/}
-        <Sidebar userName={user.name}/>
+    return (
+        <div className="flex h-screen overflow-hidden text-white">
+            {/* Sidebar Desktop */}
+            <Sidebar userName={user.name} />
 
-        <div className="flex flex-1 flex-col overflow-hidden">
-            {/* HEADER MOBILE */}
-            <MobileSidebar />
-        
-            <main className="flex-1 overflow-y-auto bg-app-background">
-                <div className="containter max-w-full px-4 px-6">
-                    {children}
-                </div>
-            </main>
+            <div className="flex flex-1 flex-col overflow-hidden">
+                {/* HEADER MOBILE */}
+                <MobileSidebar />
+
+                <main className="flex-1 overflow-y-auto bg-app-background">
+                    <div className="container max-w-full px-4 px-6">
+                        {children}
+                    </div>
+                </main>
+            </div>
         </div>
-
-    </div>
-    )
+    );
 }
