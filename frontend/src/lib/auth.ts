da9e1f7321d99ec80a817aa6b6d0d58209735639
Agstgrss <augustograssi0101@gmail.com>
2026-03-18 15:21:26 -0300

    adjust auth

--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -7,55 +7,55 @@ import { redirect } from "next/navigation";
 
 const COOKIE_NAME = "token_hamburgueria";
 
-export async function getToken(): Promise< string | undefined >{
+export async function getToken(): Promise<string | undefined> {
     const cookieStore = await cookies();
-    return cookieStore.get(COOKIE_NAME)?.value
+    return cookieStore.get(COOKIE_NAME)?.value;
 }
 
-export async function setToken(token: string){
+export async function setToken(token: string) {
     const cookieStore = await cookies();
     cookieStore.set(COOKIE_NAME, token, {
         httpOnly: true,
-        maxAge: 60 * 60 *  24 * 30,
+        maxAge: 60 * 60 * 24 * 30,
         path: '/',
         sameSite: true,
         secure: process.env.NODE_ENV === "production",
-    })
+    });
 }
 
-export async function removeToken(){
-    const cookieStore = await  cookies();
-    cookieStore.delete(COOKIE_NAME)
+export async function removeToken() {
+    const cookieStore = await cookies();
+    cookieStore.delete(COOKIE_NAME);
 }
 
-export async function getUser(): Promise <User | null>{
-    try{
+export async function getUser(): Promise<User | null> {
+    try {
         const token = await getToken();
 
-        if(!token){
+        if (!token) {
             return null;
         }
 
         const user = await apiClient<User>("/me", {
-            token: token
-        })
+            token
+        });
 
-        return user;
+        return user ?? null;
 
-    }catch(err){
-        console.log(err);
-        return null
+    } catch (err) {
+        console.log("Erro ao buscar usuário:", err);
+        return null;
     }
 }
 
-export async function requiredAdmin(): Promise <User | null>{
+export async function requiredAdmin(): Promise<User> {
     const user = await getUser();
 
-    if(!user){
+    if (!user) {
         redirect("/login");
     }
 
-    if(user?.role !== "ADMIN") {
+    if (user.role !== "ADMIN") {
         redirect("/access-denied");
     }
 
