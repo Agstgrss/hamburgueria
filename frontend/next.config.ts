79da50d7e843ba373ac83c17a374fb4194a1f7c6
Agstgrss <augustograssi0101@gmail.com>
2026-03-19 10:15:04 -0300

    i will try

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,17 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol:"https",
-        hostname: "",
-        port: "",
-      },
-    ],
-  },
 };
 
 export default nextConfig;
