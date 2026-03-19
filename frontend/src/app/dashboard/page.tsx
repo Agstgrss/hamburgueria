6bc96870ddbfdc4140bc78e779a66a18d8580661
Agstgrss <augustograssi0101@gmail.com>
2026-03-18 23:06:22 -0300

    vermelho

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,12 +1,15 @@
 import { Orders } from "@/components/dashboard/orders";
 import { getToken } from "@/lib/auth";
+import { redirect } from "next/navigation";
+
+export const dynamic = "force-dynamic";
 
 export default async function Dashboard() {
     const token = await getToken();
 
     if (!token) {
-    throw new Error("Token não encontrado");
-}
+        redirect("/login");
+    }
 
-return <Orders token={token} />;
+    return <Orders token={token} />;
 }
