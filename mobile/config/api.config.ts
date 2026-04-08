594ba5eed303310df465c3b5089a0358d457b78b
Agstgrss <augustograssi0101@gmail.com>
2026-04-08 11:08:50 -0300

    change api mobilide value

--- mobile/config/api.config.ts
+++ mobile/config/api.config.ts
@@ -1,6 +1,8 @@
-//local BASE_URL: "http://localhost:3334",
+//local BASE_URL: "http://localhost:3333",
 
 export const API_CONFIG = {
     BASE_URL: "http://localhost:3333",
     TIMEOUT: 12000, 
 }
+
+
