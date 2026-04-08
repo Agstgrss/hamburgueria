d2d224ba1ecc3c4805f5873ad4da24c2099c7362
Agstgrss <augustograssi0101@gmail.com>
2026-04-08 11:22:20 -0300

    CHANGE API MOBILE VALUE

--- mobile/config/api.config.ts
+++ mobile/config/api.config.ts
@@ -1,8 +1,8 @@
-//local BASE_URL: "http://localhost:3333",
+const isDev = true //mudar para true para desenvolvimento local, false para produção
 
 export const API_CONFIG = {
-    BASE_URL: "http://localhost:3333",
-    TIMEOUT: 12000, 
+    BASE_URL: isDev
+        ? "http://localhost:3333"
+        : "https://backend-production-2d0c.up.railway.app",
+    TIMEOUT: 12000,
 }
-
-
