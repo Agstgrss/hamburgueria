bfc64290cd0836d7dd0ac29f094b9add15df131a
Agstgrss <augustograssi0101@gmail.com>
2026-04-16 12:50:58 -0300

    mobyle adjust

--- mobile/config/api.config.ts
+++ mobile/config/api.config.ts
@@ -1,7 +1,6 @@
-//local BASE_URL: "http://localhost:3333",
-
 export const API_CONFIG = {
-    BASE_URL: "https://backend-production-2d0c.up.railway.app",
+    BASE_URL: "https://hamburgueria-60xc.onrender.com",
+    // LOCAL BASE_URL: "http://localhost:3333",
     TIMEOUT: 12000, 
 }
 
